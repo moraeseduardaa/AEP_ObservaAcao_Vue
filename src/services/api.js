47a8d3c3bdab16480e98faec4bb44f21a50f8401
Eduarda Moraes <moraeseduarda72@gmail.com>
2026-06-05 10:36:02 -0300

    correcao de rota

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.DEV
-    ? '/api'
-    : 'https://observaacao-api.onrender.com',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+  baseURL: '/api',
+  headers: { 'Content-Type': 'application/json' }
 })
 
 export default {
