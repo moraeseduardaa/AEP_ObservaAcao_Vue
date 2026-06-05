80a3ac28918665d248470154265b72bfae326905
Eduarda Moraes <moraeseduarda72@gmail.com>
2026-06-05 09:17:48 -0300

    render para vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' }
+  baseURL: import.meta.env.DEV
+    ? '/api'
+    : 'https://observaacao-api.onrender.com',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
 
 export default {
